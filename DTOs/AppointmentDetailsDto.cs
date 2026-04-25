08814440f53cbb69cfaf9ca6b6629e9e1f2c9242
Tatiana Ivanova <s33285@pjwstk.edu.pl>
2026-04-25 14:03:16 +0200

    Created DTOs for appointments

---
+++ DTOs/AppointmentDetailsDto.cs
@@ -0,0 +1,23 @@
+﻿namespace APBD_TASK6.DTOs
+{
+    public class AppointmentDetailsDto
+    {
+        public int IdAppointment { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public string Reason { get; set; } = string.Empty;
+        public string? InternalNotes { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public string PatientFirstName { get; set; } = string.Empty;
+        public string PatientLastName { get; set; } = string.Empty;
+        public string PatientEmail { get; set; } = string.Empty;
+        public string PatientPhoneNumber{ get; set;} = string.Empty;
+
+        public string DoctorFirstName { get; set; } = string.Empty;
+        public string DoctorLastName { get; set; } = string.Empty;
+        public string DoctorLicenseNumber { get; set; } = string.Empty;
+        public string Specialization { get; set; } = string.Empty;    
+    
+    }
+}
